44642f97312db3d92ab2abb6498dd6ac94a130d6
MuhammadUsman92 <106598317+MuhammadUsman92@users.noreply.github.com>
2022-08-08 10:30:55 +0500

    Initial commit

---
+++ src/test/java/com/muhammadusman92/configurationserver/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhammadusman92.configurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
